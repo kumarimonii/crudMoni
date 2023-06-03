dcc75788ccb3ff9925fe140bd51f221797921f58
kumarimonii <120780404+kumarimonii@users.noreply.github.com>
2023-06-03 05:34:46 +0530

    inital

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,7 +33,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 import {MatIconModule} from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
-import { environment } from 'src/envirnonment/environment';
+import { environment } from 'src/envirnonment/envirnonment';
 import {MatFormFieldModule} from '@angular/material/form-field';
 
 import {MatInputModule} from '@angular/material/input';
--- src/envirnonment/envirnonment.ts
+++ src/envirnonment/envirnonment.ts
@@ -1,3 +1,11 @@
 export const environment={
     production:false,
+     firebaseConfig :{
+        apiKey: "AIzaSyAgk68eBNmI2Qi6EHNC-7kWVajFepm7d0A",
+        authDomain: "badran-eed37.firebaseapp.com",
+        projectId: "badran-eed37",
+        storageBucket: "badran-eed37.appspot.com",
+        messagingSenderId: "218065936619",
+        appId: "1:218065936619:web:b47014277e004b5f1e1eae"
+      }
 }
